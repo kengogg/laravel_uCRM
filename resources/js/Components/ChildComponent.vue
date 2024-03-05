421ce1d6a1b92ab1fe5d42f811c097a2e4867c77
kengo <kengoshimazaki@icloud.com>
2024-03-06 00:13:34 +0900

    Inertia-vue.js

---
+++ resources/js/Components/ChildComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button @click="sendMessageToParent">Send Message to Parent</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const sendMessageToParent = () => {
+  const newMessage = 'Hello from Child!';
+  emit('sendMessage', newMessage);
+};
+</script>
---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <GuestLayout>
+  <div>
+    <p>{{ message }}</p>
+    <ChildComponent @sendMessage="updateMessage" />
+  </div>
+</GuestLayout>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from '@/Component/ChildComponent.vue';
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+
+const message = ref('');
+
+const updateMessage = (newMessage) => {
+  message.value = newMessage;
+};
+</script>
