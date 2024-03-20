60ac6b5efff4bbf8f44c444ec94ba9a08967ea4a
kengo <kengoshimazaki@icloud.com>
2024-03-20 10:28:35 +0900

    vue-chat-3

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AnalysisController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Customer;
@@ -13,6 +14,10 @@ use App\Models\Customer;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('auth:sanctum')
+->get('/analysis', [AnalysisController::class, 'index'])
+    ->name('api.analysis');
 
 Route::middleware('auth:sanctum')->get('/searchCustomers', function(Request $request){
     return Customer::searchCustomers($request->search)->select('id', 'name', 'kana', 'tel')->paginate(50);
